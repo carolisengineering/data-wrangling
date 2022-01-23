93811d854e1c56ae091992b0d011c1e139781576
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-01-22 18:11:19 -0600

    initial data scrape

--- scrape-data/scrape_data/cpsdemographics/cpsdemographics/spiders/CPSDemographics.py
+++ scrape-data/scrape_data/cpsdemographics/cpsdemographics/spiders/CPSDemographics.py
@@ -3,6 +3,16 @@ import scrapy
 class CPSDemographics(scrapy.Spider):
     name = 'cpsdemographics'
     start_urls = ['https://www.cps.edu/about/district-data/demographics/']
+    file_directory = 'files/'
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         self.logger.info('A response from %s just arrived!', response.url)
+        page = response.url.split("/")[-2]
+        filename = f'{self.file_directory}cpsdemographics-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
