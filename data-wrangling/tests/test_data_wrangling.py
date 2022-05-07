0d84fce80443de430a024460ebec833218451ea2
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-07 10:49:15 -0500

    test authentication routes

--- data-wrangling/tests/test_data_wrangling.py
+++ data-wrangling/tests/test_data_wrangling.py
@@ -14,3 +14,18 @@ def test_profile(test_client):
     resp = test_client.get('/profile')
     assert resp.status_code == 200
     assert resp.text == 'Profile'
+
+def test_login(test_client):
+    resp = test_client.get('/login')
+    assert resp.status_code == 200
+    assert resp.text == 'Login'
+
+def test_signup(test_client):
+    resp = test_client.get('/signup')
+    assert resp.status_code == 200
+    assert resp.text == 'Signup'
+
+def test_logout(test_client):
+    resp = test_client.get('/logout')
+    assert resp.status_code == 200
+    assert resp.text == 'Logout'
