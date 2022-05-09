7ae48ccee2de5698333a7511ff6055efd500bb92
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-09 06:33:03 -0500

    use postgres test db

--- data-wrangling/data_wrangling/blueprints/auth/models.py
+++ data-wrangling/data_wrangling/blueprints/auth/models.py
@@ -2,6 +2,7 @@ from db.db import db
 from flask_login import UserMixin
 
 class Users(db.Model, UserMixin):
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
--- data-wrangling/tests/conftest.py
+++ data-wrangling/tests/conftest.py
@@ -3,14 +3,17 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__),os.pardir,"data_wrangling"))
 
 from data_wrangling.data_wrangling import create_app
-from db.db import db
+from data_wrangling.blueprints.auth.models import Users
+from data_wrangling.db.db import db
+
+TEST_DB_URL = 'postgresql://test_user:test_password@localhost:5432/data_wrangling_test'
 
 
 @pytest.fixture
 def test_client():
     test_app = create_app()
     test_app.config['TESTING'] = True
-    test_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    test_app.config['SQLALCHEMY_DATABASE_URI'] = TEST_DB_URL
 
     with test_app.test_client() as test_client:
         with test_app.app_context():
