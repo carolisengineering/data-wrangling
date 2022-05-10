35a6d602d9bf60a864ba9b34b2648dde71a31f65
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-09 20:11:07 -0500

    test user flow

--- data-wrangling/tests/conftest.py
+++ data-wrangling/tests/conftest.py
@@ -8,12 +8,17 @@ from data_wrangling.db.db import db
 
 TEST_DB_URL = 'postgresql://test_user:test_password@localhost:5432/data_wrangling_test'
 
+@pytest.fixture
+def test_user():
+    test_user = Users(name='stacy', email='stacy@email.com', password='password')
+    return test_user
 
 @pytest.fixture
 def test_client():
     test_app = create_app()
     test_app.config['TESTING'] = True
     test_app.config['SQLALCHEMY_DATABASE_URI'] = TEST_DB_URL
+    db.app = test_app
 
     with test_app.test_client() as test_client:
         with test_app.app_context():
