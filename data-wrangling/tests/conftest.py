fa5c77b6dec76eda1acf0c2a5e2d13a0be2728c6
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-09 20:50:28 -0500

    add database teardown

--- data-wrangling/tests/conftest.py
+++ data-wrangling/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__),os.pardir,"data_wrangling"))
+import sqlalchemy as sa
 
 from data_wrangling.data_wrangling import create_app
 from data_wrangling.blueprints.auth.models import Users
@@ -14,13 +15,43 @@ def test_user():
     return test_user
 
 @pytest.fixture
-def test_client():
+def test_client(db_session):
     test_app = create_app()
     test_app.config['TESTING'] = True
     test_app.config['SQLALCHEMY_DATABASE_URI'] = TEST_DB_URL
     db.app = test_app
+    db.session = db_session
 
     with test_app.test_client() as test_client:
         with test_app.app_context():
             db.create_all()
         yield test_client
+
+@pytest.fixture(scope="session")
+def engine():
+    return sa.create_engine(TEST_DB_URL)
+
+
+@pytest.fixture(scope="session")
+def tables(engine):
+    Users.metadata.create_all(engine)
+    yield
+    Users.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def db_session(engine,tables):
+    """Returns an sqlalchemy session, and after the test tears down everything properly."""
+    connection = engine.connect()
+    # begin the nested transaction
+    transaction = connection.begin()
+    # use the connection with the already started transaction
+    session = sa.orm.Session(bind=connection)
+
+    yield session
+
+    session.close()
+    # roll back the broader transaction
+    transaction.rollback()
+    # put back the connection to the connection pool
+    connection.close()
--- data-wrangling/tests/test_user_flow.py
+++ data-wrangling/tests/test_user_flow.py
@@ -1,6 +1,7 @@
 from flask import url_for
+import psycopg
 
-def test_signup_new_user(test_client, test_user, postgresql):
+def test_signup_new_user(test_client, test_user):
     user_input_data = {
         'email': test_user.email,
         'name': test_user.name,
