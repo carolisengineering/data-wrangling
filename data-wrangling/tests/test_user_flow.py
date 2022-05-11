3b70d0596e0371317f74b46f2ff83065c5848b75
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-10 20:25:19 -0500

    verify database contents before and after assertions in tests

--- data-wrangling/tests/test_user_flow.py
+++ data-wrangling/tests/test_user_flow.py
@@ -1,8 +1,16 @@
 from flask import url_for
 import psycopg
 
-def test_signup_new_user(test_client, test_user):
-    # assert user is not already in db
+from data_wrangling.blueprints.auth.models import Users
+
+
+def test_signup_new_user(test_client, test_user, db_session):
+    # new user is not already in db
+    query = db_session.query(Users)
+    new_user = query.filter(Users.email == test_user.email)
+    new_user_exists_in_db = new_user.one_or_none()
+    assert not new_user_exists_in_db
+    
     user_input_data = {
         'email': test_user.email,
         'name': test_user.name,
@@ -12,8 +20,18 @@ def test_signup_new_user(test_client, test_user):
     assert resp.status_code == 200
     assert resp.request.path == url_for('auth.login')
 
-def test_signup_existing_user(test_client, test_user):
-    # need to assert test_user is already in db
+    # after signup, new user is in db
+    new_user_after_post = query.filter(Users.email == test_user.email)
+    new_user_exists_in_db_after_post = new_user_after_post.one_or_none()
+    assert new_user_exists_in_db_after_post
+
+def test_signup_existing_user(test_client, test_user, db_session):
+    # existing user is already in db
+    query = db_session.query(Users)
+    existing_user = query.filter(Users.email == test_user.email)
+    existing_user_exists_in_db = existing_user.one_or_none()
+    assert existing_user_exists_in_db
+
     user_input_data = {
         'email': test_user.email,
         'name': test_user.name,
@@ -22,10 +40,15 @@ def test_signup_existing_user(test_client, test_user):
     resp = test_client.post('/auth/signup', data=user_input_data, follow_redirects=True)
     assert resp.status_code == 200
     assert resp.request.path == url_for('auth.signup')
-    # assert error message is in response
+    assert b'a user with that email address already exists' in resp.data
 
-def test_login_existing_user(test_client, test_user):
-    # assert user is already in db
+def test_login_existing_user(test_client, test_user, db_session):
+    # existing user is already in db
+    query = db_session.query(Users)
+    existing_user = query.filter(Users.email == test_user.email)
+    existing_user_exists_in_db = existing_user.one_or_none()
+    assert existing_user_exists_in_db
+    
     user_input_data = {
         'email': test_user.email,
         'name': test_user.name,
@@ -35,8 +58,13 @@ def test_login_existing_user(test_client, test_user):
     assert resp.status_code == 200
     assert resp.request.path == url_for('main.profile')
 
-def test_login_existing_user_bad_password(test_client, test_user_bad_password):
-    # assert user is already in db
+def test_login_existing_user_bad_password(test_client, test_user_bad_password, db_session):
+    # existing user is already in db
+    query = db_session.query(Users)
+    existing_user = query.filter(Users.email == test_user_bad_password.email)
+    existing_user_exists_in_db = existing_user.one_or_none()
+    assert existing_user_exists_in_db
+
     user_input_data = {
         'email': test_user_bad_password.email,
         'name': test_user_bad_password.name,
@@ -45,4 +73,4 @@ def test_login_existing_user_bad_password(test_client, test_user_bad_password):
     resp = test_client.post('/auth/login', data=user_input_data, follow_redirects=True)
     assert resp.status_code == 200
     assert resp.request.path == url_for('auth.login')
-    # assert error message is in response
+    assert b'please check your login details and try again.' in resp.data
