c662632b3d19c9024d7068a2f892d0afe33625e2
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-11 19:53:09 -0500

    show logged in user's name

--- data-wrangling/data_wrangling/blueprints/main/views.py
+++ data-wrangling/data_wrangling/blueprints/main/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, url_for, redirect, render_template, flash
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__, template_folder="templates/main")
 
@@ -8,8 +9,9 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', name=current_user.name)
 
 @main.app_errorhandler(404)
 def page_not_found(e):
