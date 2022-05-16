f262e7bb1a108942332363ab1aeaa4c35d732c8c
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-16 16:57:47 -0500

    add google login constants

--- data-wrangling/data_wrangling/constants.py
+++ data-wrangling/data_wrangling/constants.py
@@ -5,3 +5,8 @@ DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_NAME = 'data_wrangling'
 FLASK_SECRET_KEY = 'flask_secret_key'
 PRODUCTION_DATABASE_URL = os.environ.get('PRODUCTION_DATABASE_URL')
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
